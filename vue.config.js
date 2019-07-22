a9e777b084e75e435eab086169b02c12204eec3c
王湘赣 <wangxianggan@hnjing.com>
2019-07-22 15:20:37 +0800

    a

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const ConfigPlugin = require('./ConfigPlugin')
 module.exports = {
   publicPath: '/demo2',
   configureWebpack: {
+    optimization: {
+      minimize: false
+    },
     externals: {
       vue: 'Vue',
       'vue-router': 'VueRouter'
